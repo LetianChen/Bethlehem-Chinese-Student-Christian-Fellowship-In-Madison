05b29174d48a1cc0ba4d90721fafc46ae8c5827a
LetianChen <letichen@umich.edu>
2020-06-20 21:00:41 -0500

    Updates

---
+++ precache-manifest.9f844f310a6d47067b7a2a1cdaa099d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e660b0b05c630458dd41a57b06ce3f",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/index.html"
+  },
+  {
+    "revision": "09fb84b262b8c1f1be42",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/css/main.da481134.chunk.css"
+  },
+  {
+    "revision": "156ccaa13f6d4b3bb8a7",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/js/2.6c334d58.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/js/2.6c334d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fb84b262b8c1f1be42",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/js/main.877c47b6.chunk.js"
+  },
+  {
+    "revision": "d9d5e448e36c0453a329",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/js/runtime-main.9c22df1f.js"
+  },
+  {
+    "revision": "18cf81c44b2e72b241ac44abb4cecc8e",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/media/background1.18cf81c4.jpg"
+  },
+  {
+    "revision": "600cf6fdc2e0b0028abe154fd23f10a7",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/media/bible.600cf6fd.jpeg"
+  },
+  {
+    "revision": "d27252e0f3c29deb651308afd2ed7c53",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/media/study.d27252e0.jpeg"
+  },
+  {
+    "revision": "77cf02bd377468aca2cf16b027919b63",
+    "url": "/Bethlehem-Chinese-Student-Christian-Fellowship-In-Madison/static/media/testimony.77cf02bd.jpeg"
+  }
+]);
